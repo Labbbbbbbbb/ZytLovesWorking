b96f0c66985e9fcf8524ff3daeff04e78ac1750d
labbbbbbbbb <724757025@qq.com>
2025-08-28 21:15:29 +0800

    8.28

--- K230/samplecode/yolo_test.py
+++ K230/samplecode/yolo_test.py
@@ -9,19 +9,18 @@ if __name__=="__main__":
     display_mode="lcd"
     rgb888p_size=[640,640]
 
-    display_size=[640,480]
 
     # 路径可以自行修改适配您自己的模型
-    kmodel_path="/data/best16.kmodel"
+    kmodel_path="/data/best.kmodel"
     labels = ["redlaser"]
     confidence_threshold = 0.3
     nms_threshold=0.3
     model_input_size=[640,640]
     # 初始化PipeLine
-    pl=PipeLine(rgb888p_size=rgb888p_size,display_size=display_size,display_mode=display_mode)
+    pl=PipeLine(rgb888p_size=rgb888p_size,display_mode=display_mode)
     pl.create()
     # 初始化YOLOv5实例
-    yolo=YOLOv5(task_type="detect",mode="video",kmodel_path=kmodel_path,labels=labels,rgb888p_size=rgb888p_size,model_input_size=model_input_size,display_size=display_size,conf_thresh=confidence_threshold,nms_thresh=nms_threshold,max_boxes_num=50,debug_mode=0)
+    yolo=YOLOv5(task_type="detect",mode="video",kmodel_path=kmodel_path,labels=labels,rgb888p_size=rgb888p_size,model_input_size=model_input_size,conf_thresh=confidence_threshold,nms_thresh=nms_threshold,max_boxes_num=50,debug_mode=0)
     yolo.config_preprocess()
     try:
         while True:
---
+++ MachineLearning/Yolo/tools/X-AnyLabeling-env/Readme.md
@@ -0,0 +1,10 @@
+Resources:
+
+[X-AnyLabeling/docs/en/get_started.md at main · CVHub520/X-AnyLabeling](https://github.com/CVHub520/X-AnyLabeling/blob/main/docs/en/get_started.md)
+
+安装完成后运行只需
+
+```
+python anylabeling/app.py
+```
+
